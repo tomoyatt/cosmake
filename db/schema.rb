1c1a156b54510beaeddc1fed491690c70eaf7f41
tomoyatt <t.trance0208@gmail.com>
2022-09-02 13:19:59 +0000

    create test_user_account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_142053) do
+ActiveRecord::Schema.define(version: 2022_09_02_130242) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_09_01_142053) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 #管理者アドレス&パスワード
 Admin.create!(
+   name: 'コスメイク運営部',
    email: 'admin@cosmake.com',
    password: 'adminadmin',
 )
