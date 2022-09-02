33eeca999a0f5eef70e08348f69a4614c4a8b6e9
tomoyatt <t.trance0208@gmail.com>
2022-09-02 13:01:39 +0000

    create admin_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  
+  def authenticate_user
+    @user = current_user
+    if @user == nil
+      redirect_to new_user_session_path
+    end
+  end
+  
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,7 @@
 class Public::UsersController < ApplicationController
+  
+  before_action :authenticate_user
+  
   def show
   end
 
